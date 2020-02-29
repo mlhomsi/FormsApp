b1676b9ee4173551b0e35b0a3fa1ea3491d9b12e
mlhomsi <mlhomsi@gmail.com>
2020-02-29 01:27:38 -0300

    Text fields controlling

--- flutterforms/lib/main.dart
+++ flutterforms/lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "FormsApp", home: Home()));
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -11,68 +12,125 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController firstController = TextEditingController();
+  TextEditingController secondController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    firstController.text = "";
+    secondController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados!";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _submit() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return appBody();
-  }
-}
-
-Widget appBody() {
-  return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Formulário:"),
-        backgroundColor: Colors.blue,
+        title: Text("Formulário"),
         centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: _resetFields,
           )
         ],
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Icon(Icons.laptop_mac, size: 120.0, color: Colors.blue),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Número 01:",
-                labelStyle: TextStyle(color: Colors.blue, fontSize: 25.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Icon(Icons.laptop_mac,
+                    size: 120.0, color: Colors.lightBlueAccent),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Número 02:",
-                labelStyle: TextStyle(color: Colors.blue, fontSize: 25.0),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Campo 01",
+                    labelStyle: TextStyle(color: Colors.lightBlueAccent)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 25.0),
+                controller: firstController,
+                // ignore: missing_return
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Por favor, complete.";
+                  }
+                },
               ),
-            ),
-            Container(
-                height: 100.0,
-                padding: EdgeInsets.only(top: 50.0),
-                child: RaisedButton(
-                  child: Text(
-                    "Buton",
-                    style: TextStyle(fontSize: 25.0, color: Colors.white),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Campo 02",
+                    labelStyle: TextStyle(color: Colors.lightBlueAccent)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 25.0),
+                controller: secondController,
+                // ignore: missing_return
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Por favor, complete.";
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Campo 03",
+                    labelStyle: TextStyle(color: Colors.lightBlueAccent)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 25.0),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Por favor, complete.";
+                  }
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _submit();
+                      }
+                    },
+                    child: Text(
+                      "Enviar",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                    color: Colors.lightBlueAccent,
                   ),
-                  color: Colors.blue,
-                  onPressed: () {},
-                )),
-            Padding(
-                padding: EdgeInsets.only(top: 35.0),
-                child: Text(
-                  "Info",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.blue, fontSize: 25.0),
-                ))
-          ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 10.0),
+              child:  Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 25.0),
+              )
+                ,)
+            ],
+          ),
         ),
-      ));
+      ),
+    );
+  }
 }
